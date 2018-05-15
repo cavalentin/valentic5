1151bd587254f915da4ff5c7a763e15767d81f90
cavalentin <36392181+cavalentin@users.noreply.github.com>
2018-05-15 17:48:10 -0400

    Update final.cpp

--- final/final.cpp
+++ final/final.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
 #include <fstream>
-#include <string>
-#include <vector>
 using namespace std;
 
 #include <final.xml>
 
 int main()
 {
-  
+  ofstream final.xml;
+  final.xml.open ("example.txt");
+  final.xml << "Writing this to a file.\n";
+  final.xml.close();
+  return 0;
 }
